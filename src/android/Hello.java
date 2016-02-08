ae734ef9cb9a97db3b16d0fb74fb22fe94be46ab
shamarakioward <shamarakioward@gmail.com>
2016-02-08 01:10:09 -0400

    Update Hello.java

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -31,7 +31,7 @@ public class Hello extends CordovaPlugin {
         this.wifiManager = (WifiManager) cordova.getActivity().getSystemService(Context.WIFI_SERVICE);
     }
     
-    private String downloadUrl(String myurl) throws IOException 
+    private int downloadUrl(String myurl) throws IOException 
     {
         InputStream is = null;
         // Only display the first 500 characters of the retrieved
@@ -48,12 +48,8 @@ public class Hello extends CordovaPlugin {
             // Starts the query
             conn.connect();
             int response = conn.getResponseCode();
-            Log.d("log", "The response is: " + response);
-            is = conn.getInputStream();
-
-            // Convert the InputStream into a string
-            String contentAsString = readIt(is, len);
-            return contentAsString;
+            
+            return response;
 
         // Makes sure that the InputStream is closed after the app is
         // finished using it.
