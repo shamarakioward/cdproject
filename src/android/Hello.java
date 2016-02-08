d991e40d334a51a186d42003e5f2e4cb794e3cb6
shamarakioward <shamarakioward@gmail.com>
2016-02-08 01:05:20 -0400

    Update Hello.java

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -13,6 +13,8 @@ import android.net.wifi.SupplicantState;
 import android.content.Context;
 import android.util.Log;
 import org.json.JSONObject;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Hello extends CordovaPlugin {
     private WifiManager wifiManager;
@@ -22,6 +24,39 @@ public class Hello extends CordovaPlugin {
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
         this.wifiManager = (WifiManager) cordova.getActivity().getSystemService(Context.WIFI_SERVICE);
+    }
+    
+    private String downloadUrl(String myurl) throws IOException 
+    {
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 500;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 );
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d("log", "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+        // Makes sure that the InputStream is closed after the app is
+        // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            } 
+        }
     }
 
     @Override
