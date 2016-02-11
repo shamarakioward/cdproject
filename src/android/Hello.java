66e8500c3452bc3334a8c2bd47e08647f59ebbdf
shamarakioward <shamarakioward@gmail.com>
2016-02-11 02:33:56 -0400

    Update Hello.java

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -20,6 +20,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 public class Hello extends CordovaPlugin {
     private WifiManager wifiManager;
